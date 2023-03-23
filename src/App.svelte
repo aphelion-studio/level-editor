5b16e975a08efeb68ec9e30d570ab9aee6df251e
Changyuan Lin <coreballium@outlook.com>
2023-03-23 01:06:22 -0400

    Draw more accurate ellipses, planets, suns

--- src/App.svelte
+++ src/App.svelte
@@ -20,14 +20,32 @@
   </div>
   <svg {viewBox} bind:this={svg}>
     {#each level.planets as planet}
-      <ellipse
-        cx={planet.orbit.x}
-        cy={planet.orbit.y}
-        rx={planet.orbit.a}
-        ry={planet.orbit.b}
-        fill="none"
+      <path
+        d="M {planet.orbit.x - planet.orbit.a} {-planet.orbit.y}
+           C {planet.orbit.x - planet.orbit.a} {-planet.orbit.y - planet.orbit.b * 0.6},
+             {planet.orbit.x - planet.orbit.a * 0.6} {-planet.orbit.y - planet.orbit.b},
+             {planet.orbit.x} {-planet.orbit.y - planet.orbit.b}
+           S {planet.orbit.x + planet.orbit.a} {-planet.orbit.y - planet.orbit.b * 0.6}
+             {planet.orbit.x + planet.orbit.a} {-planet.orbit.y}
+           S {planet.orbit.x + planet.orbit.a * 0.6} {-planet.orbit.y + planet.orbit.b}
+             {planet.orbit.x} {-planet.orbit.y + planet.orbit.b}
+           S {planet.orbit.x - planet.orbit.a} {-planet.orbit.y + planet.orbit.b * 0.6}
+             {planet.orbit.x - planet.orbit.a} {-planet.orbit.y}"
         stroke="white"
+        fill="transparent"
       />
+    {/each}
+    {#each level.planets as planet}
+      <!-- TODO: Add support for _t_ -->
+      <circle
+        cx={planet.orbit.x - planet.orbit.a}
+        cy={-planet.orbit.y}
+        r={planet.radius}
+        fill="red"
+      />
+    {/each}
+    {#each level.suns as sun}
+      <circle cx={sun.x} cy={sun.y} r={sun.radius} fill="yellow" />
     {/each}
   </svg>
 </main>
